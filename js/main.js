827f80947373ec015bad52f188ea6941aace575b
ArtemSidun <tomas1772@gmail.com>
2015-10-01 18:13:07 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,55 @@
+var headUrl = 'http://theoldnewbie.github.io/ajax_v1.01/';
+var htmlList = ['simple1.html','simple2.html','simple3.html'];
+var imgList = ['img/Troll1.jpg','img/Troll2.jpg','img/Troll3.jpg'];
+
+var ajaxResult = 0;
+var $divLeft = $('<div>');
+$divLeft.css({
+	position: 'absolute',
+	width: '600px',
+	height: '600px',
+	left: '-600px',
+	marginLeft: '50px'
+});
+var $divRight = $('<div>');
+$divRight.css({
+	position: 'absolute',
+	width: '600px',
+	height: '600px',
+	right: '-600px',
+	marginRight: '50px'
+});
+$.ajax({
+	url: headUrl + htmlList[0],
+	success: function(data) {
+		$divLeft.html(data);	
+		$(document.body).append($divLeft);
+		
+		animateDivs();
+	},
+	error: function(data) {
+	  console.error(data);
+	}
+});
+
+$.ajax({
+	url: headUrl + imgList[0],
+	success: function(data) {
+		
+		$divRight.html('<img src = ' + this.url + '>');	
+		$(document.body).append($divRight);
+		
+		animateDivs();
+	},
+	error: function(data) {
+	  console.error(data);
+	}
+});
+
+function animateDivs() {
+	ajaxResult++;
+	if (ajaxResult === 2) {
+		$divLeft.animate({left: 0}, 1500);
+		$divRight.animate({right: 0}, 1500);
+	};
+}
